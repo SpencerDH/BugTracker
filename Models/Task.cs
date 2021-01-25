a6465107720cd4f92a54eb956a6482eefe034bd2
Spencer Hall <sd77hall@gmail.com>
2021-01-25 16:36:14 -0500

    Making last update before trying to add the new project-task-issue structure

---
+++ Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class Task
+    {
+        // Properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        // Navigation properties
+        public ICollection<BugPage> BugPages { get; set; }
+    }
+}
